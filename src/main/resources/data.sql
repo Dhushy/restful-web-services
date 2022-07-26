7990d92dd5737b837863988d488414fce20b71d8
Dhushy <archunandhushanthini@gmail.com>
2022-07-26 21:53:03 +0530

    initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user values(1000, CURRENT_TIMESTAMP(), 'AB');
+insert into user values(1001, CURRENT_TIMESTAMP(), 'Jill');
+insert into user values(1002, CURRENT_TIMESTAMP(), 'Jam');
+insert into post values(11001, 'My First Post', 1000);
+insert into post values(11002, 'My Second Post', 1000);
